732c4e2f5c6479d7557def1dea8a2252cd1a3f68
shaheerkhan8476 <khanshaheer13@yahoo.com>
2024-02-18 03:57:09 -0800

    Sound Removal
    Removed sound aspect of UI

--- Gallery/MeditateWelcomeView.swift
+++ Gallery/MeditateWelcomeView.swift
@@ -13,30 +13,19 @@ import RealityKitContent
 struct MeditateWelcomeView: View {
     @Binding var immersiveSpaceActive: Bool
     @Binding var prompt: String
-    @State private var choice: String = "Exist"
+   
     @Environment(\.openWindow) private var openWindow
-    let choices = ["Exist", "Hear Sound"]
+   
     @Environment(\.dismiss) private var dismiss
     var exitEnvironment: () -> Void
     @State private var showBreathingAnimation = false
     
     var body: some View {
                     VStack {
-                Text("Do you want to exist or listen to sound?")
-                Picker("Choose an option:", selection: $choice) {
-                    ForEach(choices, id: \.self) { choice in
-                        Text(choice).tag(choice)
-                    }
-                }
-                .foregroundColor(.white)
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
                         Button("Continue") {
                             dismiss();
                             openWindow(id: "Breathing")
                         }
-                        
-
                 Button("Exit Environment") {
                     exitEnvironment()
                 }
